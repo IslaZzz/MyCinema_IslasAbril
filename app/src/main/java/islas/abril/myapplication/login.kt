a81e0ccf22370e206affdc844408aace96c97c72
IslaZzz <99843612+IslaZzz@users.noreply.github.com>
2026-02-14 20:03:49 -0700

    Inicio sesión listo

--- app/src/main/java/islas/abril/myapplication/login.kt
+++ app/src/main/java/islas/abril/myapplication/login.kt
@@ -1,6 +1,8 @@
 package islas.abril.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,13 @@ class login : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val goButton = findViewById<Button>(R.id.btnGo)
+        val intent = Intent(this, MainActivity::class.java)
+
+        goButton.setOnClickListener {
+            startActivity(intent)
+        }
+
     }
 }
